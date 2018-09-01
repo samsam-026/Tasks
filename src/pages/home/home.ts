2761bfdd522606f0a2f680f0e3ba33c1e65e7a53
Smeesh <rahman.sameeha@gmail.com>
2018-09-01 15:31:33 +0530

    Integrated firestore to all 4 CRUD actions

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { NavController, AlertController, App } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { LoginPage } from '../login/login';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
   selector: 'page-home',
@@ -11,22 +12,38 @@ export class HomePage {
 
   taskList = [];
   taskName: string = "";
+  userId: any;
+  fireStoreTaskList: any;
+  fireStoreList: any;
   @ViewChild('taskInput') input;
 
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public afAuth: AngularFireAuth, public app: App) {
-
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public afAuth: AngularFireAuth, public app: App, public firestore: AngularFirestore) {
   }
 
   ionViewDidLoad() {
     setTimeout(() => {
       this.input.setFocus();
     }, 500);
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.userId = user.uid
+        this.fireStoreTaskList = this.firestore.doc<any>('users/' + this.userId).collection('tasks').valueChanges();
+        this.fireStoreList = this.firestore.doc<any>('users/' + this.userId).collection('tasks');
+      }
+    });
+    //this.userId = this.afAuth.auth.currentUser.uid;
+
+    //this.fireStoreTaskList = this.firestore.doc<any>('users/' + this.userId).collection('tasks');
   }
 
   addTask() {
     if (this.taskName.length > 0) {
       let task = this.taskName;
-      this.taskList.push(task);
+      let id = this.firestore.createId();
+      this.fireStoreList.doc(id).set({
+        id: id,
+        taskName: task
+      });
       this.taskName = "";
     }
     this.input.setFocus();
@@ -45,14 +62,14 @@ export class HomePage {
       inputs: [{ name: 'editTask', placeholder: 'Task' }],
       buttons: [
         { text: 'Cancel', role: 'cancel' },
-        { text: 'Update', handler: data => { this.taskList[index] = data.editTask; } }
+        { text: 'Update', handler: data => { this.fireStoreList.doc(index).update({ taskName: data.editTask }); } }
       ]
     });
     alert.present();
   }
 
   deleteTask(index) {
-    this.taskList.splice(index, 1);
+    this.fireStoreList.doc(index).delete();
   }
 
 }
